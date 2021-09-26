76e930b47952411fac888d92c247445db1b9b578
pycgo <55949132+pycgo@users.noreply.github.com>
2021-09-26 14:34:51 +0800

    Create day.sh
    每20天执行一次脚本

---
+++ shell/day.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+first_date=1632637915
+date_now=`date +%s`
+interval_day=$((date_now-first_date))
+if (($interval_day > 1728000 ))
+then
+    echo "yes" #已换成需要执行的语句
+    sed -i "s/^first_date=.*/first_date=$date_now/g" aaa.sh
+fi
