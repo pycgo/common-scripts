d12a82e736bd00abfcfbee02afb30ef9bd4dec47
pycgo <55949132+pycgo@users.noreply.github.com>
2021-12-28 17:04:47 +0800

    Create countWords.go

---
+++ go/countWords.go
@@ -0,0 +1,30 @@
+//统计文件里面 字母计数大于1的打印
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Println(os.Stderr, err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+
+	}
+}
