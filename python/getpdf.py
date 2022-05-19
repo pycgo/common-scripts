27e4174b0884c5a4d769d5b5baf9efce5760257d
pycgo <55949132+pycgo@users.noreply.github.com>
2022-05-19 17:53:46 +0800

    Update getpdf.py

--- python/getpdf.py
+++ python/getpdf.py
@@ -8,14 +8,17 @@ python -m pip install --upgrade pymupdf
 
 input_file = r'/Users/zxx/Desktop/src.pdf'
 doc=fitz.open(input_file)
-#select 选择的是一个页数列表
+
 list1 = []
-#142页 到 148 就写 141 148
-first_num = 132
-last_num = 137
+first_num = 142
+last_num = 148
+
 for i in range(first_num-1,last_num):
     list1.append(i)
+    
+#select 选择的是一个页数列表
 doc.select(list1)
+
 doc2 = fitz.open()
 doc2.insert_pdf(doc)
 doc2.save(str(first_num)+'-'+str(last_num)+'.pdf')
