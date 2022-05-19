b77d6cd02c9223d2025613807f5f1ee0d22b27a3
pycgo <55949132+pycgo@users.noreply.github.com>
2022-05-19 17:51:22 +0800

    Update getpdf.py

--- python/getpdf.py
+++ python/getpdf.py
@@ -10,15 +10,15 @@ input_file = r'/Users/zxx/Desktop/src.pdf'
 doc=fitz.open(input_file)
 #select 选择的是一个页数列表
 list1 = []
-#142页 到 148
-for i in range(141,148):
+#142页 到 148 就写 141 148
+first_num = 132
+last_num = 137
+for i in range(first_num-1,last_num):
     list1.append(i)
-
-print(list1)
 doc.select(list1)
 doc2 = fitz.open()
 doc2.insert_pdf(doc)
-doc2.save('44.pdf')
+doc2.save(str(first_num)+'-'+str(last_num)+'.pdf')
 
 
 doc.close()
