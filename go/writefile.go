03ce57b411bb351f5d47c10c25f289dcce284dc1
pycgo <55949132+pycgo@users.noreply.github.com>
2023-01-17 16:36:32 +0800

    Create writefile.go

---
+++ go/writefile.go
@@ -0,0 +1,30 @@
+/*
+  读取命令行参数 并写入文件
+*/
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reflect"
+)
+
+func main() {
+	argsWriteFile(os.Args[1:])
+}
+
+func argsWriteFile(args []string) {
+	fmt.Println(args, reflect.TypeOf(args))
+	file, err := os.OpenFile("bbb.txt", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("文件打开失败", err)
+	}
+	//及时关闭file句柄
+	defer file.Close()
+	write := bufio.NewWriter(file)
+	for _, value := range args {
+		write.WriteString(value + "\n")
+	}
+	write.Flush()
+}
