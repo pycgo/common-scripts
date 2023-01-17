280c21cc09f3eaeefd0b8a46caea2705dcbfd7be
pycgo <55949132+pycgo@users.noreply.github.com>
2023-01-17 16:37:02 +0800

    Update writefile.go

--- go/writefile.go
+++ go/writefile.go
@@ -7,7 +7,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"reflect"
 )
 
 func main() {
@@ -15,7 +14,6 @@ func main() {
 }
 
 func argsWriteFile(args []string) {
-	fmt.Println(args, reflect.TypeOf(args))
 	file, err := os.OpenFile("bbb.txt", os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		fmt.Println("文件打开失败", err)
